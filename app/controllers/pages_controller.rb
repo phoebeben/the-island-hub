a27b668b56f4897c9c1eff759afa973c6b653ee6
Alice Jackson <alicejj92@gmail.com>
2023-05-29 16:23:23 +0100

    created database and migrated user table

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,4 @@
 class PagesController < ApplicationController
-  def home
+  def landing
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  devise_for :users
+  root to: "pages#landing"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
