aee5204b2368afca01442f3d337e3f8148ce2e69
Camillo Giugliano <camillogiugliano98000@gmail.com>
2023-05-30 14:04:53 +0100

    added guest validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,5 @@ class Booking < ApplicationRecord
 
   validates :start_date, presence: true
   validates :end_date, presence: true
+  validates :guests, numericality: { only_integer: true }, inclusion: { in: 1..50 }
 end
