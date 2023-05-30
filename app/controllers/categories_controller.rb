a96b31cba1790b7a15ab7f28f0d52f89f7861157
Alice Jackson <alicejj92@gmail.com>
2023-05-30 12:24:25 +0100

    removed available from islands

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
   def show
+    @category = Category.find[params[:id]]
+    @islands = Island.where
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_164405) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_30_111823) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,7 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_164405) do
     t.string "location"
     t.float "price"
     t.text "description"
-    t.boolean "available"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
