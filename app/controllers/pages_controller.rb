55e39b767521fb40644cedf330013a955c2c79a9
Phoebe Benbow <phoebe.benbow@hotmail.co.uk>
2023-05-31 15:32:54 +0100

    changed pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,4 @@
 class PagesController < ApplicationController
   def home
   end
-
-<<<<<<< HEAD
-  def index
-    @bookings = current_user.bookings
-=======
-  def show
-    @booking = Booking.find(params[:id])
-    @island = Island.find(@booking.island_id)
-    @user = current_user
->>>>>>> 080c37a79e012334b144e6826c06fb3ea4185aad
-  end
 end
